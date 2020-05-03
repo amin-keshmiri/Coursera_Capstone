3e4a7d4ef08d27604c0c797bd781d9aaeccef148
amin-keshmiri <64733761+amin-keshmiri@users.noreply.github.com>
2020-05-03 17:11:22 +0430

    Add files via upload

--- Coursera_Capstopne project.py
+++ Coursera_Capstopne project.py
@@ -1,1 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# #this project will be used for the capstone project. 
+# #then the projec will be uploaded to the git hub
+
+# In[2]:
+
+
+import pandas as pd 
+
+
+# In[3]:
+
+
+import numpy as np 
+
+
+# In[4]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
 
